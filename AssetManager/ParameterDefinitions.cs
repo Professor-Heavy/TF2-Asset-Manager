1716480c414115872faee872642edee262328544
Professor Heavy <sourcefilmmakerman@gmail.com>
2019-08-28 16:40:46 +0100

    Added MaterialParameterType class and simplified MaterialParameter class.
    For a future update featuring more flexible XML parsing.

--- AssetManager/ParameterDefinitions.cs
+++ AssetManager/ParameterDefinitions.cs
@@ -10,81 +10,69 @@ namespace AssetManager
 {
     public class MaterialParameter
     {
-        private string paramName; // Parameter's name
         private string parameter; // Parameter
         private string paramType; // Parameter's data type
-        private string paramValue; // Parameter's value
+        private dynamic paramValue; // Parameter's value
         private int paramForce;
-        // 0 = If compatible with the shader used (todo), ALWAYS use this parameter.
-        // 1 = Only overwrite existing parameters- do not write to file if the parameter isn't there.
-        // 2 - Do not overwrite, only use where the parameter is not present.
-        private int randomizerChance; // Chance of being applied to the material (determined at runtime)
-        private float randomizerOffset; // Offset of value in randomization
         private List<string[]> proxyParameterArray = new List<string[]>();
         private List<string> shaderFilterArray = new List<string>();
         private List<string> randomChoiceArray = new List<string>();
         private int shaderFilterMode;
-        public string ParamName { get => paramName; set => paramName = value; }
-        public string Parameter { get => parameter; set => parameter = value; }
-        public string ParamType { get => paramType; set => paramType = value; }
-        public dynamic ParamValue { get => paramValue; set => paramValue = value; }
-        public int ParamForce { get => paramForce; set => paramForce = value; }
-        public int RandomizerChance { get => randomizerChance; set => randomizerChance = value; }
-        public float RandomizerOffset { get => randomizerOffset; set => randomizerOffset = value; }
-        public List<string[]> ProxyParameterArray { get => proxyParameterArray; set => proxyParameterArray = value; }
-        public List<string> ShaderFilterArray { get => shaderFilterArray; set => shaderFilterArray = value; }
-        public int ShaderFilterMode { get => shaderFilterMode; set => shaderFilterMode = value; }
-        public List<string> RandomChoiceArray { get => randomChoiceArray; set => randomChoiceArray = value; }
+        public string ParamName { get; set; }
+        public string Parameter { get; set; }
+        public string ParamType { get; set; }
+        public dynamic ParamValue { get; set; }
+        public int ParamForce { get; set; }
+        public int RandomizerChance { get; set; }
+        public float RandomizerOffset { get; set; }
+        public List<string[]> ProxyParameterArray { get; set; }
+        public List<string> ShaderFilterArray { get; set; }
+        public int ShaderFilterMode { get; set; }
+        public List<string> RandomChoiceArray { get; set; }
 
         public MaterialParameter(string name,
                                  string parameter,
                                  string type,
-                                 string value,
+                                 dynamic value,
                                  int force = 0,
                                  int chance = 100,
                                  float offset = 0.0f,
-                                 List<string[]> proxyParameters = null,
                                  List<string> shaderFilters = null,
-                                 int filterMode = 0,
-                                 List<string> randomChoices = null)
+                                 int filterMode = 0)
         {
-            paramName = name;
-            this.parameter = parameter;
-            paramType = type;
-            paramValue = value;
-            paramForce = force;
-            randomizerChance = chance;
-            randomizerOffset = offset;
-            if (proxyParameters == null)
-            {
-                proxyParameterArray = new List<string[]>
-                {
-                    new string[2]
-                };
-            }
-            else
-            {
-                proxyParameterArray = proxyParameters;
-            }
-
+            ParamName = name;
+            Parameter = parameter;
+            ParamType = type;
+            ParamValue = value;
+            ParamForce = force;
+            RandomizerChance = chance;
+            RandomizerOffset = offset;
             if (shaderFilters == null)
             {
-                shaderFilterArray = new List<string>();
+                ShaderFilterArray = new List<string>();
             }
             else
             {
-                shaderFilterArray = shaderFilters;
+                ShaderFilterArray = shaderFilters;
             }
             shaderFilterMode = filterMode;
+        }
+    }
 
-            if (randomChoices == null)
-            {
-                randomChoiceArray = new List<string>();
-            }
-            else
-            {
-                randomChoiceArray = randomChoices;
-            }
+    public class MaterialParameterType
+    {
+        public string parameterName;
+        public string parameterInternalName;
+        public Type parameterType;
+        public bool isParameterArray;
+        public string[] parameterKeys;
+        public MaterialParameterType(string parameterName,
+                                     string parameterInternalName,
+                                     Type parameterType,
+                                     bool isParameterArray,
+                                     string[] parameterKeys)
+        {
+
         }
     }
 }
