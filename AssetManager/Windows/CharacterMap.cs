9929911468440c153a560a322793a48b728c6193
Professor Heavy <sourcefilmmakerman@gmail.com>
2022-09-19 21:53:02 +0100

    Add incomplete character map concept

---
+++ AssetManager/Windows/CharacterMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssetManager.Windows
+{
+    public partial class CharacterMap : Form
+    {
+        public CharacterMap()
+        {
+            InitializeComponent();
+        }
+
+        private void CharacterMap_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < char.MaxValue; i++)
+            {
+
+            }
+        }
+    }
+}
